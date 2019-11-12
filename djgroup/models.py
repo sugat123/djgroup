dfc02581e1eb5d4359bb1d731e3142fb5570331c
Sugat Paneru <sugatp454@gmail.com>
2019-11-12 12:21:57 +0545

    changes

--- djgroup/models.py
+++ djgroup/models.py
@@ -104,7 +104,7 @@ class AboutUs(models.Model):
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='team')
+    image = models.ImageField(upload_to='team', null=True, blank=True)
     post = models.CharField(max_length=100)
     description = models.TextField()
     facebook_link = models.URLField()
